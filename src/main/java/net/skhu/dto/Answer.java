e68b0852f17d4ffe3d7db87fd04e60d2c0ae4e11
irissun9602 <iris9602@naver.com>
2018-06-29 15:10:13 +0900

    feat: add answer.java

---
+++ src/main/java/net/skhu/dto/Answer.java
@@ -0,0 +1,42 @@
+package net.skhu.dto;
+
+public class Answer {
+
+	int id;
+	int question_id;	//외래키
+	String user_id;//외래키
+	String title;
+	String content;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getQuestion_id() {
+		return question_id;
+	}
+	public void setQuestion_id(int question_id) {
+		this.question_id = question_id;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
