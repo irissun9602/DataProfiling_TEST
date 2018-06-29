e25c7add9db06e78750856af1dae25d63d03072c
irissun9602 <iris9602@naver.com>
2018-06-29 15:11:39 +0900

    feat: Add Columns.java

---
+++ src/main/java/net/skhu/dto/Columns.java
@@ -0,0 +1,28 @@
+package net.skhu.dto;
+
+public class Columns {
+	
+	int ordinal_position;
+	String column_name;
+	String data_type;
+	public int getOrdinal_position() {
+		return ordinal_position;
+	}
+	public void setOrdinal_position(int ordinal_position) {
+		this.ordinal_position = ordinal_position;
+	}
+	public String getColumn_name() {
+		return column_name;
+	}
+	public void setColumn_name(String column_name) {
+		this.column_name = column_name;
+	}
+	public String getData_type() {
+		return data_type;
+	}
+	public void setData_type(String data_type) {
+		this.data_type = data_type;
+	}
+	
+	
+}
